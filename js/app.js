17a394ec458af661076134fc0e747e6b60913c8c
Angelu S. Legaspi <legaspi.angelu.k178@gmail.com>
2021-04-18 00:37:13 +0800

    Validating Fields part2

--- js/app.js
+++ js/app.js
@@ -29,5 +29,36 @@ function validateField(){
    let errors;
 
    //Validate the Length of the Field
-   validateLength(this)
+   validateLength(this);
+
+   //Validate the email
+   if(this.type === 'email'){
+       validateEmail(this);
+   }
+}
+//Validate the Length  of the field
+function validateLength(field){
+    if(field.value.length > 0){
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }
+    else{
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+//Validate email (checks for @ in the value)
+
+function validateEmail(field){
+    let emailText = field.value;
+    //check if the emailText contains the @sign
+    if(emailText.indexOf('@') !== -1){
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    }
+    else{
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+    
 }
